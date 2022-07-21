4038c677f993ad873e4af2f276c4ac19fb786c6e
AlexJackson01 <ajjackson22@gmail.com>
2022-07-21 18:04:16 +0100

    Additional styling for landing page

--- MoodTrack/components/LandingPage.js
+++ MoodTrack/components/LandingPage.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Linking, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 // import AppLoading from 'expo';
 import { useFonts, RobotoSlab_300Light, RobotoSlab_400Regular } from '../node_modules/@expo-google-fonts/roboto-slab';
 
@@ -10,26 +12,46 @@ export default function LandingPage() {
     RobotoSlab_400Regular,
   });
 
+  useEffect(() => {
+
+    getToken();
+  
+  }, [])
+
+const getToken = () => {
+let urlParams = new URLSearchParams(window.location.hash.replace("#", "?"));
+let token = urlParams.get("access_token");
+window.localStorage.setItem("token", token);
+setToken(token);
+}
+
 //   if (!fontsLoaded) {
 //     return <AppLoading />;
 //   }
 
+const CLIENT_ID = '32724f1f215c486993fb9e886fdce8e1';
+const REDIRECT_URI = 'http://localhost:19002';
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const RESPONSE_TYPE = 'token';
+const SCOPES = "streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
   return (
+    <LinearGradient colors={['#7e71f5', '#9f6ad6']} style={styles.body}>
     <View style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{flex:1}}>
         <View style={styles.body}>
           <View style={styles.container}>
             <Image style={styles.moodtrackLogo} source={require('../images/MoodTrack_logo.png')} />
             <Text style={styles.titleText}>Take time to reflect and discover tracks to fit any mood</Text>
             <Image style={styles.brainIcon} source={require('../images/musicbrain.png')} />
             <Text style={styles.secondaryText}>Login with:</Text>
-            <Image style={styles.spotifyLogo} source={require('../images/Spotify_Logo.png')} />
-
+            <TouchableOpacity onPress={() => Linking.openURL('http://google.com')}>
+                <Image style={styles.spotifyLogo} source={require('../images/Spotify_Logo.png')} />
+            </TouchableOpacity>
             <StatusBar style="auto" />
           </View>
         </View>
-      </ScrollView>
     </View>
+    </LinearGradient>
 
 
   );
@@ -38,7 +60,7 @@ export default function LandingPage() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: 'linear-gradient(rgba(126,113,245,1), rgba(159,106,214,1))',
+    // backgroundColor: 'linear-gradient(45deg, rgba(179,245,113,1), rgba(159,106,214,1))',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -46,17 +68,24 @@ const styles = StyleSheet.create({
     margin: 40,
     flex: 1,
     backgroundColor: '#FFFCEF',
-    width: '75%',
+    marginTop: 100,
+    marginBottom: 100,
+    width: '85%',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 20,
-    boxShadow: '1px 1px 0px #C7D0D8, 2px 2px 0px #C7D0D8, 3px 3px 0px #C7D0D8, 4px 4px 0px #C7D0D8, 5px 5px 0px #C7D0D8, 6px 6px 0px #C7D0D8, 7px 7px 0px #C7D0D8',
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    shadowOffset: {width: 2, height: 2},
+    shadowRadius: 10,
+    elevation: 8,
+    // boxShadow: '1px 1px 0px #C7D0D8, 2px 2px 0px #C7D0D8, 3px 3px 0px #C7D0D8, 4px 4px 0px #C7D0D8, 5px 5px 0px #C7D0D8, 6px 6px 0px #C7D0D8, 7px 7px 0px #C7D0D8',
   },
   titleText: {
     padding: 20,
     textAlign: 'center',
     fontSize: 18,
-    // fontFamily: 'RobotoSlab_300Light'
+    fontFamily: 'Roboto'
   },
   moodtrackLogo: {
     height: 180,
@@ -69,7 +98,8 @@ const styles = StyleSheet.create({
   },
   secondaryText: {
     fontSize: 16,
-    marginTop: 20
+    marginTop: 20,
+    fontFamily: 'Roboto'
   },
   spotifyLogo: {
     height: 40,
