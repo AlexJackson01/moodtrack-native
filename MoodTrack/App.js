14245f95e7c5272ed3010b97e856f3608091329c
AlexJackson01 <ajjackson22@gmail.com>
2022-07-21 14:15:12 +0100

    Building homepage - added styling and text

--- MoodTrack/App.js
+++ MoodTrack/App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { AppLoading } from "expo-app-loading";
+
+import {
+  useFonts,
+  RobotoSlab_300Light,
+  RobotoSlab_400Regular,
+} from '@expo-google-fonts/roboto-slab';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    RobotoSlab_300Light,
+    RobotoSlab_400Regular,
+
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{flex:1}}>
+        <View style={styles.body}>
+          <View style={styles.container}>
+            <Text style={styles.titleText}>Take time to reflect and discover tracks to fit any mood</Text>
+            <StatusBar style="auto" />
+          </View>
+        </View>
+      </ScrollView>
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'linear-gradient(rgba(126,113,245,1), rgba(159,106,214,1))',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    margin: 40,
+    flex: 1,
+    backgroundColor: '#FFFCEF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    boxShadow: '1px 1px 0px #C7D0D8, 2px 2px 0px #C7D0D8, 3px 3px 0px #C7D0D8, 4px 4px 0px #C7D0D8, 5px 5px 0px #C7D0D8, 6px 6px 0px #C7D0D8, 7px 7px 0px #C7D0D8',
+  },
+  titleText: {
+    padding: 20,
+    textAlign: 'center',
+    fontSize: 18,
+    fontFamily: 'RobotoSlab_300Light'
+  }
 });
