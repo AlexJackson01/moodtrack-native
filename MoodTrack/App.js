b66372cb678ec490daec09173256393817b2b0f4
AlexJackson01 <ajjackson22@gmail.com>
2022-07-22 11:34:02 +0100

    Added transitions to routing

--- MoodTrack/App.js
+++ MoodTrack/App.js
@@ -2,13 +2,18 @@ import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import { fromLeft } from 'react-navigation-transitions';
 import LandingPage from './components/LandingPage';
 import FindTracks from './components/FindTracks';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
+
+export default function App({ navigation }) {
 
   const [token, setToken] = useState("");
   const [trackList, setTrackList] = useState([]);
@@ -18,10 +23,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login'>
+        <Stack.Screen name='Login' options={{cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid}}>
           {(props) => <LandingPage {...props} token={token} setToken={setToken} setTrackList={setTrackList} />}
         </Stack.Screen>        
-        <Stack.Screen name='Tracks'>
+        <Stack.Screen name='Tracks' options={{cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid}}>
           {(props) => <FindTracks {...props} token={token} setToken={setToken} />}
         </Stack.Screen>
       </Stack.Navigator>   
